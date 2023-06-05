7f1e7b86e9abd9185e172ae893d6a778a7f839eb
mostafa <mostafaa_rahmani@outlook.com>
2023-06-05 16:25:18 +0330

    added loading in example project

--- src/example/app.jsx
+++ src/example/app.jsx
@@ -5,8 +5,9 @@ import { NftSlider } from "../../dist/esm/index";
 function App() {
   return (
     <NftSlider
-      chain="SOLANA"
-      collection="6Rk3N5qRR2tZSwhCXZ5MsBJTRkhioYH1bSc4g5qBf2rh"
+      chain="POLYGON"
+      collection="0x670fd103b1a08628e9557cd66b87ded841115190"
+      loadingElement={<div>My loading element</div>}
     />
   );
 }
